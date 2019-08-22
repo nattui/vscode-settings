7ceea0108283f3d5fdd970cebe5780400e14fb6b
nguyen-nhat <natt.lefevre@gmail.com>
2019-08-21 21:53:45 -0700

    Add extension.sh

---
+++ extension.sh
@@ -0,0 +1,23 @@
+# https://code.visualstudio.com/docs/editor/extension-gallery#_command-line-extension-management
+# TODO: Automate extensions list
+
+code --install-extension ahmadawais.shades-of-purple
+code --install-extension CodeStream.codestream
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.code-runner
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension ms-python.python
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode.cpptools
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension oderwat.indent-rainbow
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.java
+code --install-extension ritwickdey.live-sass
+code --install-extension ritwickdey.LiveServer
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wesbos.theme-cobalt2
+code --install-extension zhuangtongfa.Material-theme
